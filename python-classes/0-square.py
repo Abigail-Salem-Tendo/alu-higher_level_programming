8bcbe826888e32ff7b1817433b6c2c0563168dae
Abigail Salem <salemabigail10@gmail.com>
2025-07-12 03:40:13 -0700

    Having a private instance attribute

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """This module has just one class that is empty"""
 class Square:
     """This is an empty class that defines a square"""
---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""This modlue hase a private instance module called size"""
+class Square:
+    """This has a size as part of the module"""
+    def __init__(self, size):
+        """ Initializes a new size attribute"""
+        self.__size = size
+
