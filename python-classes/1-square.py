36dd2cbb1d148d9e115e44520022aef7a346f744
Abigail Salem <salemabigail10@gmail.com>
2025-07-12 03:54:11 -0700

    private attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""
+This modlue hase a private instance module called size
+"""
 
 
-"""This modlue hase a private instance module called size"""
 class Square:
     """This has a size as part of the module"""
     def __init__(self, size):
-        """ Initializes a new size attribute"""
+        """Initializes a new size attribute"""
         self.__size = size
-
