f040be145a24120a03cbc60eccdbfdad97c91377
Abigail Salem <salemabigail10@gmail.com>
2025-07-15 13:01:30 -0700

    Correcting the txt files

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -23,6 +23,6 @@ class BaseGeometry:
         raises two errors typeerror and valueerror
         """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
