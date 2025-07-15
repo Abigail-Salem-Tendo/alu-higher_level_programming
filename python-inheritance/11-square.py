81c3a0a03e4d5501a7f06e1f7a0c19d71ff87996
Abigail Salem <salemabigail10@gmail.com>
2025-07-15 12:30:28 -0700

    the tset files are giving me touble

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
         """initializes the square wiwth size"""
         self.integer_validator("size", size)
         self.__size = size
-        duper().__init__(size, size)
+        super().__init__(size, size)
 
     def area(self):
         """Returns the area of the square"""
