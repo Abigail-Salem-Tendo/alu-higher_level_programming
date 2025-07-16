c53a1dd57c3e3dd0848b1275a2eca12a46ed7e43
Abigail Salem <salemabigail10@gmail.com>
2025-07-16 03:23:32 -0700

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module has a function that returns an
+object (ds) represented by a json string
+"""
+
+
+def from_json_string(my_str):
+    """
+    This function returns the object
+    represented
+    """
+    return json.loads(my_str)
