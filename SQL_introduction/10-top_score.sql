5d5b56c6a1dfe0266edc52db0090afb46ec98f9e
Abigail Salem <salemabigail10@gmail.com>
2025-07-17 04:54:53 -0700

    Files

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Script that lists all the records of second_table in a specific oRDER
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- A script that drops all the records that have a score < 5
+DROP FROM second_table WHERE score <= 5;
