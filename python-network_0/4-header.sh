20b8bd48a77e7a3094b82543835fef50afd3393f
Abigail Salem Tendo <a.tendo@alustudent.com>
2025-07-21 10:57:29 +0000

    one more correction

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a get request with a custom header and displays the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
