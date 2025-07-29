389593d7b1102eda8f983c15e146f3bfc0671d42
Abigail Salem Tendo <a.tendo@alustudent.com>
2025-07-29 08:48:14 +0000

    Addes the readme.md file

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,15 +4,18 @@ Send a post request with an email and displays
 the response body (utf-8)
 """
 
+
 import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+	url = sys.argv[1]
+	value = {'email' : sys.argv[2]}
 
-data = urllib.parse.urlencode({'email': email}).encode('ascii')
+	data = urllib.parse.urlencode(value).encode('ascii')
+	req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(url, data) as response:
-    body = response.read().decode('utf-8')
-    print(body)
+	with urllib.request.urlopen(url, data) as response:
+    		body = response.read().decode('utf-8')
+    		print(body)
