13fcaa64a4ab3223ca0ea7c5758dc4bca4d3eb88
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2025-09-27 12:01:34 -0700

    all tasks

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint (c) {
+    let char;
+    if (c === undefined) {
+      char = 'X';
+    } else {
+      char = c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(char.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
